412e08d73cc3c4162af86716893b6f3300a03800
mfaisal951 <muhammadfaisalf611@gmail.com>
2025-05-30 16:42:23 +0500

    my project

---
+++ engine/features.py
@@ -0,0 +1,8 @@
+# import playsound as playsound
+from playsound import playsound
+
+
+#playing Assistant sound
+def playAssistantSound():
+    music_dir="www\\assets\\audio\\start_sound.mp3"
+    playsound(music_dir)
