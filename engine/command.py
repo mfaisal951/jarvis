db7cccc17cf9e9e6c9c7e625b63245456ec0d205
mfaisal951 <muhammadfaisalf611@gmail.com>
2025-07-19 11:37:59 +0500

    this is prgram

--- engine/command.py
+++ engine/command.py
@@ -1,18 +1,39 @@
 import pyttsx3
+import speech_recognition as sr
+import pyaudio
 
 def speak(text):
-   #this for voice 
     engine = pyttsx3.init('sapi5')
-    # voices = engine.getProperty('voices')
-    # engine.setProperty('voice', voices[1].id)
-    # print(voices)
-    engine.setProperty('rate',150)
-    engine.setProperty('volume',0.9)
+    voices = engine.getProperty('voices')
+    engine.setProperty('voice', voices[0].id)
+    engine.setProperty('rate', 174)
+    print(voices)
     engine.say(text)
     engine.runAndWait()
-if __name__=="__main__":
-    message="Hello, I am your computer speaking"
-    speak("i love pakistan")    
+
+def takecommand():
+    r=sr.Recognizer()
+    with sr.Microphone() as source:
+        print("listening ....")
+        r.pause_threshold=1
+        r.adjust_for_ambient_noise(source)
+        audio=r.listen(source, 10, 6)
+    try:
+        print(" Recognization")
+        query=r.recognize_google(audio, language='en-in')
+        print(f"User Said: {query}")
+    except Exception as e:
+        return ""
+    return query.lower()
+
+text=takecommand()
+            
+        
+    
+speak(text)
+
+if __name__ == "__main__":
+    takecommand()   
 
 
 
@@ -49,4 +70,6 @@ if __name__=="__main__":
 #     os.remove(filename)
 
 # speak("I love Pakistan")
+
+
 
--- main.py
+++ main.py
@@ -6,3 +6,7 @@ playAssistantSound()
 eel.init("www")
 os.system('start msedge.exe --app="http://localhost:8000/index.html"')
 eel.start('index.html',mode=None, host='localhost', block=True)
+
+
+
+
